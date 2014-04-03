e05b6d2d2b5848ebc1c39b10f3131aac80fcf8b7
041401093 <041401093@my.central.wa.edu.au>
2014-04-03 11:27:25 +0800

    I created both classes.
    The only one I finished so far is the Kilometer one.

---
+++ PerDayRental.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 041401093
+ */
+public class PerDayRental {
+    
+    private double km;
+    private final int CHARGE_RATE_DAY = 100;
+    private double totalCost;
+    
+    PerDayRental(){
+        
+    }
+}
---
+++ PerKmRental.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 041401093
+ */
+public class PerKmRental {
+    //Declarign variables
+    private double ratePerKm = 1;
+    private double km;
+    private double totalCost;
+    //Declaring object
+    private Journey gettingKm;    
+    //Getting the number of kmfrom journey
+    PerKmRental () {
+        km = gettingKm.getKilometers();
+    }
+    //Method that computes the total cost
+    private double computeKm(){
+        totalCost = km * ratePerKm;
+        return totalCost;
+    }
+}
